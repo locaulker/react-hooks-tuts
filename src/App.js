cfab7a7e8ffc96cbe389f3de4899e4bbfab711a8
Lawrence <locaulker@gmail.com>
2020-04-27 06:37:37 -0400

    initial setup updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 
 const App = () => {
+	//const [value, setValue] = useState(initialState)
+	const [name, setName] = useState("")
+
 	return (
 		<div className="main-wrapper">
-			<h1>Level Up Dishes</h1>
+			<h1>{name}</h1>
+			<input
+				type="text"
+				onChange={(e) => setName(e.target.value)}
+				value={name}
+			/>
 		</div>
 	)
 }
